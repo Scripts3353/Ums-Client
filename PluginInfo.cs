b826a997bfdf2f42900adb72ad8e32873df001d3
Ums <166935104+Scripts3353@users.noreply.github.com>
2024-09-06 15:40:23 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace IIDKQuest
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.Ums.gorillatag.menutemplate";
+        public const string Name = "UMS CLIENT";
+        public const string Description = "Created by RealX with love <3";
+        public const string Version = "1.0.0";
+    }
+}
